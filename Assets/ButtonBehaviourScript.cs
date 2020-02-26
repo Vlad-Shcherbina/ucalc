b7d505ffa21bdadc81a47a1e27fdc4c0b73eb8a0
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-02-27 02:21:45 +0300

    Prefab for buttons

---
+++ Assets/ButtonBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviourScript : MonoBehaviour
+{
+    public string label;
+    void Start()
+    {
+        var t = this.GetComponentInChildren<TextMesh>();
+        t.text = label;
+    }
+}
---
+++ Assets/MainBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!Input.GetMouseButton(0))
+        {
+            return;
+        }
+        Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var obj = hit.collider.gameObject;
+            Debug.Log(obj.GetComponentInParent<ButtonBehaviourScript>().label);
+        }
+    }
+}
